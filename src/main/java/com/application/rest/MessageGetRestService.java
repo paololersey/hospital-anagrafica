b4bdeab162f5e153cc67ed08aa049da4eefb02be
Paolo <Paolo>
2018-03-21 18:01:50 +0100

    message gest Rest Service

---
+++ src/main/java/com/application/rest/MessageGetRestService.java
@@ -0,0 +1,22 @@
+package com.application.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+//http://localhost:8080/bank-academy/rest/message/custom
+@Path("/message")
+public class MessageGetRestService {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+
+		String result = "Custom message : " + msg;
+
+		return Response.status(200).entity(result).build();
+
+	}
+
+}
